1b99ab030e55e01ff747ed589818135a148e45eb
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-04-12 23:21:00 +0200

    feat: useDeadline hook

--- App.tsx
+++ App.tsx
@@ -1,11 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  AppState,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+let logs: string[] = [];
+
+function log(message: string) {
+  console.log(message);
+  logs.push(`${new Date().toLocaleTimeString()}: ${message}`);
+}
+
+function useDeadline(deadline: Date) {
+  const hasPassed = deadline <= new Date();
+  const [, forceRender] = React.useState({});
+
+  React.useEffect(() => {
+    log("Effect");
+
+    const ms = deadline.getTime() - Date.now();
+    if (ms <= 0) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      log("forceRender");
+      forceRender({});
+    }, ms);
+
+    return () => {
+      log("clearTimeout");
+      clearTimeout(timeoutId);
+    };
+  }, [deadline]);
+
+  return hasPassed;
+}
+
+function LogsView() {
+  const [, forceRender] = React.useState({});
+
+  React.useEffect(() => {
+    const sub = AppState.addEventListener("change", (state) => {
+      log(`AppState change ${state}`);
+      forceRender({});
+    });
+
+    return () => sub.remove();
+  });
+
+  return (
+    <View style={styles.log}>
+      {logs.map((message) => (
+        <Text key={message}>{message}</Text>
+      ))}
+    </View>
+  );
+}
 
 export default function App() {
+  const [deadline, setDeadline] = React.useState(() => new Date());
+  const hasPassed = useDeadline(deadline);
+
+  const increaseDeadline = () => {
+    logs = [];
+    const newDeadline = new Date(deadline.getTime() + 15 * 1000);
+    setDeadline(newDeadline);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <Text>Deadline {deadline.toLocaleTimeString()}</Text>
+      <Text>Has Passed: {hasPassed ? "YES" : "NO"}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={increaseDeadline}>
+        <Text style={styles.buttonTitle}>Increase Deadline</Text>
+      </TouchableOpacity>
+
+      <LogsView />
     </View>
   );
 }
@@ -13,8 +90,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  log: {
+    margin: 20,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "blue",
+    padding: 20,
+    margin: 20,
+  },
+  buttonTitle: {
+    color: "white",
   },
 });
